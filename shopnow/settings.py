09d76d9618b8ec7619b55747888ff57f8d0a5d1b
Gottam Pranadeep Reddy <pranadeepreddy17@gmail.com>
2018-07-22 21:38:20 +0530

    cros change

--- shopnow/settings.py
+++ shopnow/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'shopnowapp.apps.ShopnowappConfig',
     'rest_framework',
     'webpack_loader',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
