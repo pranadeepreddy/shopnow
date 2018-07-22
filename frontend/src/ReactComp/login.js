6c5d5ae7ec0f4228da4ce592f460418e73307fe5
Gottam Pranadeep Reddy <pranadeepreddy17@gmail.com>
2018-07-22 23:19:54 +0530

    npm build 5

--- frontend/src/ReactComp/login.js
+++ frontend/src/ReactComp/login.js
@@ -24,6 +24,7 @@ class Login extends Component{
             body: form
             })
             .then(response => {
+                console.log(response);
                 if (response.ok) {
                     return response.json();
                   } else {
